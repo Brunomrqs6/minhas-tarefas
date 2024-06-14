2d219097844302355fc10e3ee9f429f2fc306a7c
Bruno Marques <yatosad@gmail.com>
2024-06-14 17:14:08 -0300

    Commit final

---
+++ src/components/BotaoAdicionar/styles.ts
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const Circulo = styled(Link)`
+  height: 64px;
+  width: 64px;
+  background-color: #44bd32;
+  color: #fff;
+  position: fixed;
+  border-radius: 50%;
+  bottom: 40px;
+  right: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 40px;
+  text-decoration: none;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`
---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,13 @@
+import BarraLateral from '../../containers/BarraLateral'
+import Formulario from '../../containers/Formulario'
+
+const Cadastro = () => {
+  return (
+    <>
+      <BarraLateral mostrarFiltro={false} />
+      <Formulario />
+    </>
+  )
+}
+
+export default Cadastro
